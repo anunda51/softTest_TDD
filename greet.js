d1582b1ff11e43db035c0b74299f09a4dd84aa98
anunda51 <42669270+anunda51@users.noreply.github.com>
2019-01-25 17:00:11 +0700

    first commit

---
+++ greet.js
@@ -0,0 +1,17 @@
+function greet(name){
+    if(name === null){
+        return "Hello, my friend.";
+    }else if(name.constructor !== Array && name == name.toUpperCase()){
+        return "HELLO " + name + "!";
+    }else if(name.constructor == Array){
+        if(name.length > 2){
+            return "Hello " + name.slice(0,name.length - 1).join(", ") + ", and " + name.slice(name.length-1) + ".";
+        }else{
+            return "Hello " + name.join(" and ") + ".";
+        }
+    }else{
+        return "Hello, " + name + ".";
+    }
+}
+
+module.exports=greet;
